a4b46bdc77db9cf4983e7a4fd80a800272cecf36
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2024-01-16 01:04:54 +0500

    Update Center.jsx

--- src/components/Center.jsx
+++ src/components/Center.jsx
@@ -3,24 +3,24 @@ import "../components/Center.css"
 
 const Center = () => {
     return (
-        <div className="center">
+        <div id="center">
             <img src="cloud-solid.svg" alt="" className="weather-img"/>
             <div className="info">
-                <h1 className="temp">22°</h1>
-                <h2 className="city">New-York</h2>
+                <h1 className="temp">22°C</h1>
+                <b><p className="city">New-York</p></b>
                 <div className="details">
                     <div className="col">
                         <img src="water-solid.svg" alt="" className="humidity-img"/>
                         <div>
-                            <p className="humidity"><b>50%</b></p>
-                            <p><b>Humidity</b></p>
+                            <p className="humidity-proc"><b>50%</b></p>
+                            
                         </div>
                     </div>
                     <div className="col-2">
                         <img src="wind-solid.svg" alt="" className="wind-img"/>
                         <div>
-                            <p className="wind-speed"><b>15 km/h</b></p>
-                            <p><b>Wind Speed</b></p>
+                            <p className="speed"><b>15 km/h</b></p>
+                            
                         </div>
                     </div>
                 </div>
